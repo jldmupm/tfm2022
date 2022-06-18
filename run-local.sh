57ce031c352ceee23135ac007bbeb30df52732b7
José L. Doménech <jl.domenech@alumnos.upm.es>
2022-06-18 17:28:11 +0200

    fix: improve running the system in the local environment

--- run-local.sh
+++ run-local.sh
@@ -1,15 +1,28 @@
-#!/bin/sh
+#!/bin/bash
 
-echo "Starting CONTAINERS"
-cd local_runtime/
-docker-compose up -d
-cd ../
+trap cleanup EXIT
 
-sleep 10
+function cleanup {
+    echo "Stoping CONTAINERS"
+    
+    cd local_runtime/
+    docker-compose down
+    cd ../
+}
 
-echo "Starting Analysis Framework"
-poetry run ./run.sh
 
-cd local_runtime/
-docker-compose down
-cd ../
+function start_containers {
+    echo "Starting CONTAINERS"
+    cd local_runtime/
+    docker-compose up -d
+    cd ../
+}
+
+function start_analysis_framework {
+    echo "Starting Analysis Framework"
+    poetry run ./run.sh
+}
+
+start_containers
+
+start_analysis_framework
