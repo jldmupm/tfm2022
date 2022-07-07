f7936fc3af9c0aafeb3feb8fe4be9bfdbff3386d
José L. Doménech <jl.domenech@alumnos.upm.es>
2022-07-08 00:50:26 +0200

    fix get cluster client

--- analysis/cluster.py
+++ analysis/cluster.py
@@ -2,7 +2,9 @@ import dask.distributed
 
 import analysis.config as cfg
 
+client: dask.distributed.Client = dask.distributed.Client(address=cfg.get_cluster())
+
 async def get_dask_client():
-    client: dask.distributed.Client = dask.distributed.Client(address=cfg.get_cluster())
+    global client
     yield client
     #client.close()
